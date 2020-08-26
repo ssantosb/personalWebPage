45cccf1c4a0a64bfed14c8d7a0f636e8d0080967
mitooos <ma.hoyosr@uniandes.edu.co>
2020-08-26 14:57:58 -0500

    Updates

---
+++ precache-manifest.66c666dd5da10344a044182f8ce947d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e3bee3f108c6cc6240ab1ff26049ba",
+    "url": "/personalWebPage/index.html"
+  },
+  {
+    "revision": "5893a402ed819f90d110",
+    "url": "/personalWebPage/static/css/main.650ff9fc.chunk.css"
+  },
+  {
+    "revision": "197abfa0386761dcf2d3",
+    "url": "/personalWebPage/static/js/2.b3d89df1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/personalWebPage/static/js/2.b3d89df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5893a402ed819f90d110",
+    "url": "/personalWebPage/static/js/main.29c60aa2.chunk.js"
+  },
+  {
+    "revision": "3da248aa70447f8a5efd",
+    "url": "/personalWebPage/static/js/runtime-main.337db00a.js"
+  }
+]);
